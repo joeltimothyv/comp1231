942b9f7dc02030a2f9bd1f0ac1d2065e6e70f6cb
Joel Timothy Vargas <59840594+joeltimothyv@users.noreply.github.com>
2020-01-19 13:53:51 -0500

    practice

---
+++ practices/week-02/challenges/index.js
@@ -0,0 +1,15 @@
+
+console.log('it works!')
+
+
+//declaration
+var age;
+let name;
+const contact = '647 280 9773' // string data type
+const homePhone = 4165465665 // number data type
+
+age = 26
+name = 'Joel'
+
+
+
